f652be8da583dea30122e3d5f0ac4b065fa51c9c
Batuhan Özcan <batuhan.ozcan@ozu.edu.tr>
2022-03-09 10:53:17 +0300

    Update Lioness-1155.sol

--- Lioness-1155.sol
+++ Lioness-1155.sol
@@ -1110,7 +1110,7 @@ contract LIONDAO is ERC1155, EIP2981, Ownable  {
     uint256 public currentSupply = 0;
     uint256 public maxTotalSupply = 111;
 
-    constructor(address _royaltyRecipient, uint256 _royaltyAmount) EIP2981(_royaltyRecipient, _royaltyAmount) ERC1155("ipfs://link/") Ownable() {}
+    constructor(address _royaltyRecipient, uint256 _royaltyAmount) EIP2981(_royaltyRecipient, _royaltyAmount) ERC1155("ipfs://QmVNNSfUuRcyvXoZdUzweAoQzYHYuis38B1SaqnDfFWpUP/") Ownable() {}
 
    
     function supportsInterface(bytes4 _interfaceId) public view virtual override(ERC1155, EIP2981) returns (bool) {
